f89359fe85cac1f8ff24424add48240c6d320c73
mdhasnainali <mdhasnainali.01@gmail.com>
2024-02-18 13:33:36 +0600

    Checking all sub-dir also

--- main.py
+++ main.py
@@ -2,33 +2,47 @@ import os
 from filecmp import cmp
 import clearScreen
 
-current_dir = os.getcwd()
-checking_dir = os.path.join(current_dir, 'files')  # directory that need to check
-files = os.listdir(checking_dir)
-files = [file_name for file_name in files if (file_name[0]
-         != '.') and os.path.isfile(os.path.join(checking_dir,file_name))]
+WORKING_DIR = '../../../Downloads/Segmentation'
+CURRENT_DIR = os.getcwd()
 
-# print(files)
+def get_files(working_dir):
+    '''Get all the files from the root dir and the sub dirs'''
+    all_files = []
+    for (relative_dir,sub_dirs,files) in os.walk(working_dir):
+        # print(os.path.abspath(relative_dir), sub_dirs)
+        temp_path = os.path.abspath(relative_dir)
+        for file in files:
+            temp_file = os.path.join(temp_path, file)
+            if(file[0] != '.'):
+                all_files.append(temp_file)
+                # print(temp_file)
 
-
-duplicate_files = []
-
-for file1 in files:
-    for file2 in files:
-        if file1 != file2:
-            file1_path = os.path.join(checking_dir, file1)
-            file2_path = os.path.join(checking_dir, file2)
-            isDuplicate = cmp(file1_path, file2_path, shallow=False)  # shallow or deep search
-            # print(file1 + "--" + file2 + " " , isDuplicate)
-            if isDuplicate and (file2, file1) not in duplicate_files:
-                duplicate_files.append((file1, file2))
+    # print(all_files)
+    return all_files
 
 
-clearScreen.clear_screen()
-if len(duplicate_files):
-    print("Duplicate Files Found!\n")
-    print("Duplicate Files:")
-    for duplicate in duplicate_files:
-        print(duplicate)
-else:
-    print("No Duplicate Files Found!")
+def find_duplicate(files):
+    '''Compare all files and find the duplicates. Also print them.'''
+    duplicate_files = []
+    for file1 in files:
+        for file2 in files:
+            if file1 != file2:
+                isDuplicate = cmp(file1, file2, shallow=False)  # shallow or deep search
+                # print(file1 + "--" + file2 + " " , isDuplicate)
+                if isDuplicate and (file2, file1) not in duplicate_files:
+                    duplicate_files.append((file1, file2))
+
+    # Print Duplicate Files
+    clearScreen.clear_screen()
+    if len(duplicate_files):
+        print("Duplicate Files Found!\n")
+        print("Duplicate Files:\n")
+        for duplicate in duplicate_files:
+            print(duplicate)
+    else:
+        print("No Duplicate Files Found!")
+
+
+if __name__ == '__main__':
+    files = get_files(WORKING_DIR)
+    find_duplicate(files)
